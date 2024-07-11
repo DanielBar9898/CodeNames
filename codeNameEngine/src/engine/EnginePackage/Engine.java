24f9ad704102f19fe79834c68416242051457b81
Dolev Bargig <dolevbargig2@gmail.com>
2024-07-11 18:14:27 +0300

    main changes

---
+++ codeNameEngine/src/engine/EnginePackage/Engine.java
@@ -0,0 +1,23 @@
+package engine.EnginePackage;
+import GamePackage.*;
+import engine.GamePackage.Game;
+import engine.GamePackage.Team;
+
+public interface Engine {
+
+    public Game loadXmlFile (String filename);
+
+    public void showGameMenu();
+
+    public void showLoadedGameInfo(Game currentGame);
+
+    public void startGame();
+
+    public void playTurn(Team teamTurn, String hint, int numOfWordsToGuess);
+
+    public boolean playTurn(Team teamTurn ,int wordIndex,BooleanWrapper gameOver);
+
+    public void printGameStats(Game currentGame,boolean team1Turn);
+
+
+}
